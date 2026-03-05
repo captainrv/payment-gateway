dffb4371ccb4370c887345d00fdf99a9e6a66e41
captainrv <74832056+captainrv@users.noreply.github.com>
2026-03-05 15:09:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 // Import Express.js
 const express = require('express');
-
+const Razorpay = require("razorpay");
 // Create an Express app
 const app = express();
-
+const razorpay = new Razorpay({
+  key_id: "rzp_test_xxxxx",
+  key_secret: "xxxxxxxx"
+});
 // Middleware to parse JSON bodies
 app.use(express.json());
 
 // Set port and verify_token
 const port = process.env.PORT || 3000;
+
+
 
 // Route for GET requests
 app.get('/', (req, res) => {
@@ -22,6 +27,26 @@ app.post('/', (req, res) => {
   console.log(JSON.stringify(req.body, null, 2));
   res.status(200).send({ data: req.body });
 });
+
+
+
+app.get("/order", async (req, res) => {
+
+  const options = {
+    amount: 50000, // 500 rs (paise me)
+    currency: "INR",
+    receipt: "receipt_order_1"
+  };
+
+  try {
+    const order = await razorpay.orders.create(options);
+    res.json(order);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+
+});
+
 
 // Start the server
 app.listen(port, () => {
